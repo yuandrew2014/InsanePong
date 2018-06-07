db1272a525673afc767eb6b63de7828e4434e643
yuandrew2014 <league@WTS22.attlocal.net>
2018-06-06 19:58:46 -0700

    final touches and I'm done

--- src/PongPanel.java
+++ src/PongPanel.java
@@ -27,22 +27,17 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	Font titleFont4;
 	Font endFont;
 	Font arrows;
-	;
+	Font enterFont;;
 	final int MENU_STATE = 0;
 
 	final int GAME_STATE = 1;
 
 	final int END_STATE = 2;
 
-	
 	int currentState = MENU_STATE;
 	Gamemanager gm;
-	
-	 
 
-        
-	
-	public PongPanel(){
+	public PongPanel() {
 
 		try {
 			bucketImg = ImageIO.read(this.getClass().getResourceAsStream("bucket.png"));
@@ -53,30 +48,31 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			e.printStackTrace();
 		}
 
-		this.player = new Player1(400,400,100,120);
+		this.player = new Player1(400, 400, 100, 120);
 		this.t1 = new Timer(1000 / 60, this);
-		this.titleFont = new Font("Arial",Font.PLAIN,48);
-		this.titleFont2 = new Font("Arial",Font.PLAIN,24);
-		this.titleFont3= new Font("Arial",Font.PLAIN,24);
-		this.titleFont4 = new Font("Arial",Font.PLAIN,24);
-		this.endFont = new Font("Arial", Font.PLAIN,24);
-		this.arrows = new Font("Arial", Font.PLAIN,24);
+		this.titleFont = new Font("Arial", Font.PLAIN, 48);
+		this.titleFont2 = new Font("Arial", Font.PLAIN, 24);
+		this.titleFont3 = new Font("Arial", Font.PLAIN, 24);
+		this.titleFont4 = new Font("Arial", Font.PLAIN, 24);
+		this.endFont = new Font("Arial", Font.PLAIN, 24);
+		this.arrows = new Font("Arial", Font.PLAIN, 24);
+		this.enterFont = new Font("Arial", Font.PLAIN, 24);
 		this.gm = new Gamemanager(player);
-			}
+	}
 
 	@Override
 	public void paintComponent(Graphics g) {
-		
+
 		if (currentState == MENU_STATE) {
 
 			drawMenuState(g);
 		} else if (currentState == GAME_STATE) {
 
-		drawGameState(g);
+			drawGameState(g);
 
 		} else if (currentState == END_STATE) {
 
-		drawEndState(g);
+			drawEndState(g);
 
 		}
 	}
@@ -85,7 +81,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		if (currentState == MENU_STATE) {
-			//System.out.println("repaint");
+			// System.out.println("repaint");
 			updateMenuState();
 
 		} else if (currentState == GAME_STATE) {
@@ -99,8 +95,7 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		}
 
 		if (player.isAlive == false) {
-			currentState 
-			= END_STATE;
+			currentState = END_STATE;
 		}
 		repaint();
 	}
@@ -112,36 +107,34 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
-	
+
 	}
+
 	@Override
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
 			player.x -= player.speed;
-		
-		}
-		else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
 			player.x += player.speed;
-		
+
 		}
-		if(currentState == 0) {
+		if (currentState == 0) {
 			if (e.getKeyCode() == KeyEvent.VK_E) {
 				player.speed = ezspeed;
-			
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_H) {
+
+			} else if (e.getKeyCode() == KeyEvent.VK_H) {
 				player.speed = harspeed;
-				
-			}
-			else if (e.getKeyCode() == KeyEvent.VK_I) {
+
+			} else if (e.getKeyCode() == KeyEvent.VK_I) {
 				player.speed = inspeed;
-				
+
 			}
 		}
 	}
+
 	@Override
 
 	public void keyReleased(KeyEvent e) {
@@ -151,18 +144,16 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 			System.out.println(currentState);
 			currentState += 1;
 
-
 			if (currentState > END_STATE) {
 				player.isAlive = true;
 				currentState = MENU_STATE;
-				this.player = new Player1( 400,  400, 100,  120);
+				this.player = new Player1(400, 400, 100, 120);
 				this.gm = new Gamemanager(player);
 			}
-	
-	}
-			
+
 		}
-	
+
+	}
 
 	void updateMenuState() {
 		repaint();
@@ -172,29 +163,31 @@ public class PongPanel extends JPanel implements ActionListener, KeyListener {
 		g.setColor(Color.ORANGE);
 		g.fillRect(0, 0, RainGame.gameWidth, RainGame.gameHeight);
 		g.setColor(Color.BLACK);
-		
+
 		g.setFont(titleFont);
-		g.drawString("Collect the Rain",220,400);
+		g.drawString("Collect the Rain", 220, 400);
 		g.setFont(titleFont2);
-		g.drawString("Click E to initiate easy mode",250,440);
+		g.drawString("Click E to initiate easy mode", 250, 440);
 		g.setFont(titleFont3);
-		g.drawString("Click H to initiate hard mode",250,480);
+		g.drawString("Click H to initiate hard mode", 250, 480);
 		g.setFont(titleFont4);
-		g.drawString("Click I to initiate insane mode",250,520);
+		g.drawString("Click I to initiate insane mode", 250, 520);
 		g.setFont(arrows);
 		g.drawString("Use your arrow key to move", 250, 600);
+		g.setFont(enterFont);
+		g.drawString("Click enter to go to the next screen(this applies to all screens)", 100, 680);
 	}
 
 	void updateGameState() {
-gm.update();
-gm.manageEnemies();
-gm.checkCollision();
-gm.purgeRain();
+		gm.update();
+		gm.manageEnemies();
+		gm.checkCollision();
+		gm.purgeRain();
 
 	}
 
 	void drawGameState(Graphics g) {
-		 g.drawImage(PongPanel.landImg, 0,0,  800, 800, null);
+		g.drawImage(PongPanel.landImg, 0, 0, 800, 800, null);
 		g.setColor(Color.WHITE);
 		gm.draw(g);
 	}
@@ -208,8 +201,8 @@ gm.purgeRain();
 		g.fillRect(0, 0, RainGame.gameWidth, RainGame.gameHeight);
 		g.setColor(Color.DARK_GRAY);
 		g.setFont(endFont);
-		g.drawString("your score is : " + gm.score,340 , 400);
-		
+		g.drawString("your score is : " + gm.score, 340, 400);
+
 	}
 
 	void updateHighState() {
